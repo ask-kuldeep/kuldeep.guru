2fee7edf8e312e22e7fcbbd7f2cbdba9cdea86e4
Vaibhav Kaushik <68017022+vansul@users.noreply.github.com>
2020-10-26 21:41:50 +0530

    Added views counter.

---
+++ functions/count.js
@@ -0,0 +1,30 @@
+const firebase = require('firebase');
+require('firebase/firestore');
+require('dotenv').config();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+exports.handler = (_a, _b, callback) => {
+  db.collection('counter')
+    .add({
+      countable: true,
+    })
+    .then(() => {
+      callback(null, { statusCode: 200, body: '{ "success": true }' });
+    })
+    .catch((err) => {
+      callback(null, { statusCode: 200, body: '{ "error": err }' });
+    });
+};
