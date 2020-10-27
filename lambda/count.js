1acd5ba1339b077658cf9a343dfa7ca185daf750
Vaibhav Kaushik <68017022+vansul@users.noreply.github.com>
2020-10-27 16:16:07 +0530

    made some changes.

--- lambda/count.js
+++ lambda/count.js
@@ -4,23 +4,18 @@ const app = require('./app');
 
 const db = firebase.firestore(app);
 
-exports.handler = async (_event, _context, callback) => {
-  let result;
-
-  await db
-    .collection('counter')
+exports.handler = (_event, _context, callback) => {
+  db.collection('counter')
     .add({
       countable: true,
     })
     .then(() => {
-      result = callback(null, { statusCode: 200, body: '{ "success": true }' });
+      callback(null, { statusCode: 200, body: '{ "success": true }' });
     })
     .catch((error) => {
-      result = callback(null, {
+      callback(null, {
         statusCode: 200,
         body: JSON.stringify({ error }),
       });
     });
-
-  return result;
 };
--- lambda/counts.js
+++ lambda/counts.js
@@ -5,22 +5,15 @@ const app = require('./app');
 const db = firebase.firestore(app);
 
 exports.handler = async (_event, _context, callback) => {
-  let result;
+  const data = await db.collection('counter').get();
 
-  await db
-    .collection('counter')
-    .get()
-    .then((data) => {
-      const counts = data.docs
-        .map((count) => count.data())
-        .filter((count) => count.countable)
-        .length.toString();
+  const counts = await data.docs
+    .map((count) => count.data())
+    .filter((count) => count.countable)
+    .length.toString();
 
-      result = callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({ counts }),
-      });
-    });
-
-  return result;
+  return callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({ counts }),
+  });
 };
